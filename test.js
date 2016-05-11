3348fa5dec9bd167013ddc1d58af48eeaabac310
Scott <swilliams@students.stab.org>
2016-05-10 23:13:44 -0400

    view works but is ugly

--- test.js
+++ test.js
@@ -8,23 +8,35 @@ var io = require('socket.io').listen(server);
 app.engine('html', engines.hogan); // tell Express to run . html files through Hogan
 app.set('views', __dirname + '/Templates'); // tell Express where to find templates
 app.use(express.static(__dirname + '/Public'));
-
-var data = [
-		  ['Date', 'Number'],
-          ['2016/4/1',  1],
-          ['2016/4/2',  2],
-          ['2016/4/3',  10],
-          ['2016/4/4',  5]
-		  ];
+var conn = anyDB.createConnection('sqlite3://example.db.sqlite');
 
 
 app.get('/', function(request, response){
-	response.render('test.html');
+	response.render('view_Workout.html');
 
 });
 
-app.get('/test.json', function(request, response){
-	response.send(data);
+app.get('/user/:name', function(request, response){
+	var name = request.params.name;
+
+	response.render('view_Workout.html', {
+			"name": name
+	});
+});
+
+app.get('/json/:name', function(request, response){
+	var name = request.params.name;
+	var wolist = [];
+	conn.query('SELECT * FROM ' + name + ";'").on('data', function(row){
+		wolist.push(row);
+		}).on('end', function(){
+			response.send(wolist);
+	}); // probably need to make this sql injection-proof
+});
+
+
+app.get('/view', function(request, response){
+	response.render('view_Workout.html');
 });
 
 server.listen(8080, function() {
