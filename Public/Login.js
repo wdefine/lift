2f334c30b92c9ce408cec59d5c5d503f1ec09838
AndyWood <awood@students.stab.org>
2016-05-19 22:46:25 -0400

    Finished everything on createWorkout Page except for creating workout. Doing that now

--- Public/Login.js
+++ Public/Login.js
@@ -5,14 +5,14 @@ window.addEventListener('load', function(){
 		console.log("login");
 		var username = $("#UsernameArea").val();
 		var password = $("#PasswordArea").val();
-		socket.emit("Login", username, password);	
+		//do what ever needs to happen to authenticate user
 	});
 	$('#NewUserButton').on('click', function(){
 		console.log("new user");
 		var name = $("#NewUserFirstName").val() + " " + $("#NewUserLastName").val();
 		var email = $("#NewUserEmail").val();
 		var password = $("#NewUserPassword").val();
-		socket.emit("createUser", name, password);	
+		socket.emit("createUser", name, email);	
 	});
 	$('#NewAccountButton').on('click', function(){
 		$('#Login').hide();
